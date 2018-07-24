d85107885e7ef3f063672bdcad1f6c4f1a066d6f
Benjamin Siegel <benjaminsiegel@Benjamins-MacBook-Pro-2.local>
2018-07-24 09:01:17 -0400

    first commit

---
+++ signin.js
@@ -0,0 +1,16 @@
+//Challenge 1: Sign user in with gmail when user presses 'sign in with gmail'
+
+
+//Challenge 2: Display the users avatar after they have signed in
+
+
+//Challenge 3: Display the users name on the top right hand corner of the navbar after they have signed in
+
+
+//Challenge 4: Create a "Sign Out" button in the navbar that allows the user to sign Out
+
+
+// Challenge 5: When the user navigates to other pages of the website, make sure their name is still displayed in the top right hand corner, as well as the sign out button.
+
+
+// Challenge 6: When a user adds a new post, have it automatically add their email to the posting without them having to input it manually
