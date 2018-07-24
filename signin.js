5b0dc2872bac661285462509d635631e429f9bc1
marieke-thomas <32941729+marieke-thomas@users.noreply.github.com>
2018-07-24 14:12:01 -0400

    Update signin.js

--- signin.js
+++ signin.js
@@ -1,13 +1,13 @@
 //Challenge 1: Sign user in with gmail when user presses 'sign in with gmail'
 
 
-//Challenge 2: Display the users avatar after they have signed in
+//Challenge 2: Display the user's avatar after they have signed in. Hint: use setAttribute (Google it!)
 
 
-//Challenge 3: Display the users name on the top right hand corner of the navbar after they have signed in
+//Challenge 3: Display the user's name on the top right hand corner of the navbar after they have signed in
 
 
-//Challenge 4: Create a "Sign Out" button in the navbar that allows the user to sign Out
+//Challenge 4: Create a "Sign Out" button in the navbar that allows the user to sign out
 
 
 // Challenge 5: When the user navigates to other pages of the website, make sure their name is still displayed in the top right hand corner, as well as the sign out button.
